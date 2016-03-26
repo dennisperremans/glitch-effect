a9f7ae5a5956b284c32baad1cdc0260dbce48b40
Dennis Perremans <perremansdennis@gmail.com>
2016-03-26 16:43:50 +0100

    add jquery shizzle

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,22 +12,52 @@ $(function(){
         debug: true
     });
 
-    $('.glitch').mouseover(function(){
-        var $this = $(this),
-            $innerText = $this.text(),
-            $dataColor = $this.data('color'),
-            $dataBg = $this.data('bg'),
-            $dataAttr = $this.attr('data-text');
+    $(".glitch").hover(
+        function(e){
+            var $this = $(this),
+                $innerText = $this.text(),
+                $dataColor = $this.data('color'),
+                $dataBg = $this.data('bg'),
+                $dataAttr = $this.attr('data-text'),
+                $thisOffset = $this.offset(),
+                $thisX = $thisOffset.left,
+                $thisY = $thisOffset.top;
 
-        if ( $this.not('[data-text]') ) {
-            $this.attr('data-text', $innerText);
+
+            // clone element 1 and position it
+            if(!$('.cloneOne').length) {
+                var $cloneOne = $this.clone().addClass('clone cloneOne');
+                $cloneOne.appendTo('body');
+                $cloneOne.css({
+                    left: $thisX,
+                    top: $thisY
+                });
+            }
+
+            // clone element 2 and position it
+            if(!$('.cloneTwo').length) {
+                var $cloneTwo = $this.clone().addClass('clone cloneTwo');
+                $cloneTwo.appendTo('body');
+                $cloneTwo.css({
+                    left: $thisX,
+                    top: $thisY
+                });
+            }
+
+            //set the color on the cloned elements
+            if ($this.attr('data-color')) {
+                $('.cloneOne, .cloneTwo').css('color', $dataColor)
+            }
+
+            // copy text into data-text
+            if ( $this.not('[data-text]') ) {
+                $this.attr('data-text', $innerText);
+            }
+        },
+        function(e){
+            //remove cloned element onMouseLeave
+            $('.cloneOne').remove();
+            $('.cloneTwo').remove();
         }
-    });
-
-    /* ------------------------------
-        Fire functions on resize
-    ------------------------------- */
-    $( window ).resize(function() {
-
-    });
+    );
 });
